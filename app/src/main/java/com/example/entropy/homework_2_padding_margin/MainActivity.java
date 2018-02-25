42febcdf3a84aa5d4b808b2420c5e984a4d1bbfe
entropy88 <entropystarrover@gmail.com>
2018-02-25 16:54:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/entropy/homework_2_padding_margin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.entropy.homework_2_padding_margin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
